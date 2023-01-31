cc34026a115c895d26c09fe971688cc57bfe364b
Yves O <yves.otchoun@gmail.com>
2023-01-31 09:00:48 -0500

    Add test data

--- src/main/java/com/mafoya/oja/model/For.java
+++ src/main/java/com/mafoya/oja/model/For.java
@@ -2,7 +2,6 @@ package com.mafoya.oja.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
